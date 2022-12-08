0b91cb11e5fbf62eff78ef609f7918fb57095683
Suneel Ugar <ssuneel41@gmail.com>
2022-12-08 09:55:52 +0530

    Token Based login using JSON web token

---
+++ springjwttoken/README.md
@@ -0,0 +1,31 @@
+* Maven dependencies
+  <dependency>
+  <groupId>io.jsonwebtoken</groupId>
+  <artifactId>jjwt-api</artifactId>
+  <version>0.11.1</version>
+  </dependency>
+  <dependency>
+  <groupId>io.jsonwebtoken</groupId>
+  <artifactId>jjwt-impl</artifactId>
+  <version>0.11.1</version>
+  <scope>runtime</scope>
+  </dependency>
+  <dependency>
+  <groupId>io.jsonwebtoken</groupId>
+  <artifactId>jjwt-jackson</artifactId>
+  <version>0.11.1</version>
+  <scope>runtime</scope>
+  </dependency>
+
+Project Configurations:
+
+protected void configure(HttpSecurity http) throws Exception {
+http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().
+config.setAllowedHeaders(Collections.singletonList("*"));
+
+1) Write JWTTokenGeneratorFilter.java
+2) Write JWTTokenValidatorFilter.java
+
+
+![plot](./filterchain.png)
+
---
+++ springjwttoken/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -0,0 +1,80 @@
+package com.eazybytes.config;
+
+import com.eazybytes.filter.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+import java.util.Collections;
+
+@Configuration
+public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    /**
+     * From Spring Security 5.7, the WebSecurityConfigurerAdapter is deprecated to encourage users
+     * to move towards a component-based security configuration. It is recommended to create a bean
+     * of type SecurityFilterChain for security related configurations.
+     *
+     * @param http
+     * @return SecurityFilterChain
+     * @throws Exception
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().
+                cors().configurationSource(new CorsConfigurationSource() {
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                        CorsConfiguration config = new CorsConfiguration();
+                        config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+                        config.setAllowedMethods(Collections.singletonList("*"));
+                        config.setAllowCredentials(true);
+                        config.setAllowedHeaders(Collections.singletonList("*"));
+                        config.setExposedHeaders(Arrays.asList("Authorization"));
+                        config.setMaxAge(3600L);
+                        return config;
+                    }
+                }).and().csrf().disable()
+                .addFilterBefore(new RequestValidationBeforeFilter(), BasicAuthenticationFilter.class)
+                .addFilterAfter(new AuthoritiesLoggingAfterFilter(), BasicAuthenticationFilter.class)
+                .addFilterBefore(new JWTTokenValidatorFilter(), BasicAuthenticationFilter.class)
+                .addFilterAfter(new JWTTokenGeneratorFilter(), BasicAuthenticationFilter.class)
+                .addFilterAt(new AuthoritiesLoggingAtFilter(), BasicAuthenticationFilter.class)
+                .authorizeRequests()
+                .antMatchers("/myAccount").hasRole("USER")
+                .antMatchers("/myBalance").hasAnyRole("USER","ADMIN")
+                .antMatchers("/myLoans").hasRole("ROOT")
+                .antMatchers("/myCards").hasAnyRole("USER","ADMIN")
+                .antMatchers("/user").authenticated()
+                .antMatchers("/notices").permitAll()
+                .antMatchers("/contact").permitAll().and().httpBasic();
+    }
+
+    //to ignore csrf .and().csrf().ignoringAntMatchers("/notices", "/contact").and().authorizeRequests()
+
+         /*   http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated()
+                .antMatchers("./notices", "/myContacts").permitAll()
+                .and().formLogin()
+                .and().httpBasic();*/
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
